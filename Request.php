bbcb81658f23e55e630cc232fa854ae66c13486a
zappaciato <58880515+zappaciato@users.noreply.github.com>
2023-09-01 20:55:33 +0200

    Homework

--- Request.php
+++ Request.php
@@ -2,39 +2,140 @@
 
 class Request {
 
-    public string $address;
+    public string $url;
     public int $port; 
     public string $httpMethod; 
+    public array $curlData;
 
-    private function currentPort($address) {
-        // tutaj rozbijamy address zeby uzyskac info czy jest http czy https
-        $addressPort = substr($address, 0, 5);
-        if ($addressPort == "http") {
-            $port = 80;
-        } else {
+    public function getCurrentPort($url) {
+        $addressPort = substr($url, 0, 5);
+        if ($addressPort == "https") {
             $port = 442;
+        } else {
+            $port = 80;
         }
 
         return $port;
     }
 
-    private function httpMethod() {
-        
-    }
 
     private function validateData() {
 
-        if (!isset($this->address)) {
+        if (!isset($this->url)) {
             throw new Exception('No address given');
+        } 
+
+        if (!isset($this->httpMethod)) {
+            throw new Exception('No valid HTTPMethod given');
         } else {
+            // echo $this->httpMethod;
+            // ten switch robilem po to zeby zwalidowac poprawność wpisanej metody http. Mozna to inaczej ale pomyslalem, ze ten sposob bedzie ok w razie gdyby jeszcze zostaly inne rzeczy do walidacji specyficzne do kazdej z tych metod. 
+            switch (strtoupper($this->httpMethod)) {
+                
+                case 'POST': 
+                    $this->httpMethod = 'POST';
+                    break;
+
+                case 'GET':
+                    $this->httpMethod = 'GET';
+                    break;
+//tu jednak cos mi mowi, ze nie jest ok jak default zwraca incorrect method given.. ale nie mam pewnosci
+                default:
+                    throw new Exception('Incorrect httpMethod given!');
+            }
+        }
+
+    }
+
+// poniższa metoda wyswietli nam status wraz ze znaczeniem. 
+    public function statusCodeDisplay($e){
+        switch ($e) {
+            case 404:
+                $e = 404 . " Not found!!! ";
+                echo $e;
+                break;
+
+            case 405:
+                $e = 404 . " Method Not Allowed!!! ";
+                echo $e;
+                break;
             
+            case 200: 
+                $e = 200 . " OK!!! ";
+                echo $e;
+                break;
+
+            case 302:
+                $e = 302 . " Found!!! ";
+                echo $e;
+                break;
         }
     }
 
     public function sendRqs ()
     {
         $this->validateData();
-        echo "I am sending the request NOW!";
+        //tutaj wzialem pod uwage jedyne dwie metody, jesli wiecej to bym robil elseif. Po sprawdzeniu ktora to metoda odpala odpowiednia metode
+        if($this->httpMethod == "POST") {
+
+            $this->curlPOSTRqs();
+
+        } else {
+
+            $this->curlGETRqs();
+
+        }
+        
+    }
+
+    public function curlPOSTRqs() {
+
+        //dummy dane do POST
+        $postRequest = array(
+            'firstFieldData' => 'foo',
+            'secondFieldData' => 'bar'
+        );
+        $curlData = $this->curlInit($postRequest);
+
+        //nie wiem dlaczego status code display wyrzuca na poczatek linijki zamiast na koniec???
+        echo "I am sending the POST request NOW! Port: " . $this->port . " HttpMethod: " . $this->httpMethod . ' //  ' . $this->statusCodeDisplay($curlData['status_code']);
+    }
+
+
+    public function curlGETRqs()
+    {
+
+        $curlData = $this->curlInit();
+
+        echo "I am sending the GET request NOW! Port: " . $this->port . " HttpMethod: " . $this->httpMethod . ' //  ' .
+        $this->statusCodeDisplay($curlData['status_code']);
+    }
+
+    private function curlInit ($postRequest = null) {
+
+        $ch = curl_init($this->url);
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if($this->httpMethod == "POST") {
+            
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postRequest);
+            curl_setopt($ch, CURLOPT_POST, 1);
+
+            curl_close($ch);
+            // array_push();
+            $curlData = ['ch' => $ch, 'server_output' => $server_output, 'status_code' => $statusCode];
+        } else {
+
+            curl_close($ch);
+            // array_push();
+            $curlData = ['ch' => $ch, 'server_output' => $server_output, 'status_code' => $statusCode];
+        }
+
+        var_dump($curlData);
+        return $curlData;
     }
 }
 
--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
 
 include 'Request.php';
+include 'Logger.php';
+
+// $url = "https://www.google.co.uk/search?q=cow";
+$url = 'https://jsonplaceholder.typicode.com/todos/1';
+$httpMethod = "GET";
+
 
 $firstRequest = new Request();
+$firstRequest->url = $url;
+$firstRequest->httpMethod = $httpMethod;
+$firstRequest->port = $firstRequest->getCurrentPort($url);
 
-$firstRequest->address = 'www.google.pl';
+// $log = new Logger(); nie musimy jej instantiate bo metody sa static
+// print_r(Logger::info('THis is info from the Logger'));
+// Logger::debug($firstRequest);
 
 try {
     $firstRequest->sendRqs();
